123485dd2e641394923bc19b4e002e42c767ed33
Lit <2316429288@qq.com>
2022-03-27 15:08:45 +0800

    add 主页
    主页增加了搜索和视频背景

--- App.vue
+++ App.vue
@@ -1,11 +1,12 @@
 <script>
 	import Vue from 'vue';
-	
+
 	export default {
 		onLaunch() {
 			// 绑定屏幕尺寸信息
 			uni.getSystemInfo({
 				success(e) {
+					console.log(e)
 					Vue.prototype.$windowWidth = e.windowWidth;
 					Vue.prototype.$windowHeight = e.windowHeight;
 					Vue.prototype.$screenWidth = e.screenWidth;
@@ -13,19 +14,12 @@
 				}
 			});
 			// 监听屏幕变化
-			window.onresize = () => {
-				return (() => {
-					uni.getSystemInfo({
-						success(e) {
-							Vue.prototype.$windowWidth = e.windowWidth;
-							Vue.prototype.$windowHeight = e.windowHeight;
-							Vue.prototype.$screenWidth = e.screenWidth;
-							Vue.prototype.$screenHeight = e.screenHeight;
-						}
-					});
-					this.$forceUpdate();
-				}) ();
-			}
+			uni.onWindowResize(res => {
+				Vue.prototype.$windowWidth = res.size.windowWidth;
+				Vue.prototype.$windowHeight = res.size.windowHeight;
+				Vue.prototype.$screenWidth = res.size.screenWidth;
+				Vue.prototype.$screenHeight = res.size.screenHeight;
+			});
 		}
 	}
 </script>
@@ -33,7 +27,7 @@
 <style lang="scss">
 	/*每个页面公共css */
 	@import '@/static/index.scss';
-	
+
 	page {
 		background: url('@/static/image/bg_pattern.jpg');
 		background-repeat: no-repeat;
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,28 @@
 <template>
-	<view class="content">
+	<view class="content flex">
 		<mhwi-sidebar :navList="navList" @navClick="handleNavClick" />
+		<view class="page-right">
+			<view class="mhwi-search-bar flex align-center">
+				<view class="mhwi-search flex align-center" @click="$refs.mhwiSearchModel.open">
+					<text class="iconfont mhwi-search-icon" />
+				</view>
+			</view>
+			<view class="mhwi-main flex">
+				<view class="flex-8">
+					<video v-if="videoShow"
+						src="http://r9aejxg1h.hn-bkt.clouddn.com/video/%E3%80%90MHWI%E3%80%91%20%E6%80%AA%E7%89%A9%E7%8C%8E%E4%BA%BA%E4%B8%96%E7%95%8C%EF%BC%9A%E5%86%B0%E5%8E%9F%20%E9%AB%98%E7%87%83CG%E6%B7%B7%E5%89%AA%20%20%20%E6%9E%81%E8%87%B4%E8%B8%A9%E7%82%B9%20%20%20%E4%B8%87%E7%89%A9%E7%9A%86%E5%8F%AFSold%20Out%20%20%20%E9%87%8D%E6%96%B0%E5%8E%8B%E5%88%B6%E7%89%88%E6%9C%AC%20%20%20%E6%97%A0%E4%BA%8C%E5%8E%8B_%E9%AB%98%E6%B8%85%201080P.mp4"
+						autoplay muted loop :show-fullscreen-btn="false" :show-play-btn="false"
+						:show-center-play-btn="false" :show-loading="false" :enable-progress-gesture="false"
+						:controls="false" class="mhwi-main-video" />
+				</view>
+				<view class="flex-2">
+
+				</view>
+			</view>
+		</view>
+		<!-- 搜索模态框 -->
+		<mhwi-search-model ref="mhwiSearchModel" :searchData="searchData" :loading="loading" @close="searchData = []"
+			@search="search" @arrowClick="pageJump('', 'arrow', $event)" @itemClick="pageJump('', 'detail', $event)" />
 	</view>
 </template>
 
@@ -8,8 +30,9 @@
 	export default {
 		data() {
 			return {
-				navList: [
-					{
+				videoShow: false,
+				loading: false,
+				navList: [{
 						id: 0,
 						title: '魔物',
 						active: true,
@@ -135,20 +158,121 @@
 						active: false,
 						activeIcon: require('@/static/image/menu_icon.png')
 					}
-				]
+				],
+				searchData: [],
+				searchTimer: null
 			}
 		},
 		methods: {
 			handleNavClick(index) {
+				console.log(this.$windowWidth)
 				this.navList.map(item => {
 					item.active = false;
 				});
 				this.navList[index].active = true;
 			},
+			search(data) {
+				this.searchData = [];
+				if (data == '') {
+					return
+				} else {
+					this.loading = true;
+					if (this.searchTimer == null) {
+						this.searchTimer = setTimeout(() => {
+							this.searchData.push({
+								content: 'MONSTER',
+								icon: 'http://r9aejxg1h.hn-bkt.clouddn.com/icon/monster.png',
+								data: [{
+										icon: 'http://r9aejxg1h.hn-bkt.clouddn.com/icon/huanghei.png',
+										title: '煌黑龙',
+										info: '虽然有关的记录已经遗失多时，但是被称呼为「破坏的象征」，而且可以驱使各种自然力量。',
+										flag: 'ALATREON',
+										content: 'MONSTER'
+									},
+									{
+										icon: 'http://r9aejxg1h.hn-bkt.clouddn.com/icon/huanghei.png',
+										title: '煌黑龙',
+										info: '虽然有关的记录已经遗失多时，但是被称呼为「破坏的象征」，而且可以驱使各种自然力量。',
+										flag: 'ALATREON',
+										content: 'MONSTER'
+									},
+									{
+										icon: 'http://r9aejxg1h.hn-bkt.clouddn.com/icon/huanghei.png',
+										title: '煌黑龙',
+										info: '虽然有关的记录已经遗失多时，但是被称呼为「破坏的象征」，而且可以驱使各种自然力量。',
+										flag: 'ALATREON',
+										content: 'MONSTER'
+									},
+									{
+										icon: 'http://r9aejxg1h.hn-bkt.clouddn.com/icon/huanghei.png',
+										title: '煌黑龙',
+										info: '虽然有关的记录已经遗失多时，但是被称呼为「破坏的象征」，而且可以驱使各种自然力量。',
+										flag: 'ALATREON',
+										content: 'MONSTER'
+									}
+								]
+							});
+							this.loading = false;
+						}, 500);
+					} else {
+						this.searchTimer = null
+					};
+				};
+			},
+			pageJump(route, type, args) {
+				switch (type) {
+					case 'arrow':
+						break;
+					case 'detail':
+						break;
+				};
+			}
+		},
+		onLoad() {
+			if(this.$windowWidth > 1500) {
+				this.videoShow = true;
+			};
+			uni.onWindowResize(res => {
+				this.videoShow = res.size.windowWidth < 1500 ? false : true;
+			});
 		}
 	}
 </script>
 
-<style lang="scss">
-	
+<style lang="scss" scoped>
+	.page-right {
+		width: 100%;
+	}
+
+	.mhwi-search-bar {
+		width: 100%;
+		height: 8.5vh;
+		background: $mhwi-primary-light;
+		padding-left: 2vw;
+
+		.mhwi-search {
+			width: 50%;
+			height: 70%;
+			border-radius: 50rpx;
+			background: #EDEDED;
+			padding-left: 1vw;
+
+			.mhwi-search-icon::before {
+				content: '\e95c';
+				color: $mhwi-primary-light;
+				font-size: 3vh;
+			}
+		}
+	}
+
+	.mhwi-main {
+		margin: 2vh 2vw;
+		border-radius: 10rpx;
+
+		.mhwi-main-video {
+			height: 78.5vh;
+			width: 100%;
+			border-radius: 10rpx;
+		}
+	}
 </style>
